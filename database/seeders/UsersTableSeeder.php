9c7b8b1a0ed08ed14cbf42c5d02a07f7fad53725
初めてのコミット <>
2025-10-03 14:13:59 +0900

    ルーティング途中

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'username' => '早川',
+                'email' => 'takumi0303@email.com',
+                'password' => bcrypt('0000')
+            ]
+        ]);
+    }
+}
--- routes/auth.php
+++ routes/auth.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
 
-    Route::get('login', [AuthenticatedSessionController::class, 'create']);
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
     Route::get('register', [RegisteredUserController::class, 'create']);
@@ -16,5 +16,9 @@ Route::middleware('guest')->group(function () {
 
     Route::get('added', [RegisteredUserController::class, 'added']);
     Route::post('added', [RegisteredUserController::class, 'added']);
+});
 
+
+Route::middleware('auth')->group(function () {
+    Route::post('logout', [AuthenticatedSessionController::class, 'logout'])->name('logout');
 });
